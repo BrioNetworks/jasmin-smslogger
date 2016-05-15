dcf9fabccec9f0ed1d883f0e62bc65b6d4b6741d
dmt.malin <dmt.malin@gmail.com>
2016-05-15 21:02:34 +0300

    return submit_sm_bill

--- src/smslogger/sms_logger.py
+++ src/smslogger/sms_logger.py
@@ -116,15 +116,15 @@ class SmsLogger(object):
 
                     pdu_count, short_message = get_multipart_message(pdu, short_message)
 
-                    submit_sm_resp_bill = pickle.loads(headers['submit_sm_resp_bill']) \
-                        if 'submit_sm_resp_bill' in headers else None
-                    if not submit_sm_resp_bill:
-                        logger.warning(u'empty submit_sm_resp_bill in message %s' % (props['message-id'], ))
+                    submit_sm_bill = pickle.loads(headers['submit_sm_bill']) \
+                        if 'submit_sm_bill' in headers else None
+                    if not submit_sm_bill:
+                        logger.warning(u'empty submit_sm_bill in message %s' % (props['message-id'], ))
 
                     rate, uid = 0, None
-                    if submit_sm_resp_bill:
-                        rate = submit_sm_resp_bill.getTotalAmounts() * pdu_count
-                        uid = submit_sm_resp_bill.user.uid
+                    if submit_sm_bill:
+                        rate = submit_sm_bill.getTotalAmounts() * pdu_count
+                        uid = submit_sm_bill.user.uid
 
                     # Преобразуем сообщение
                     if 'data_coding' in pdu.params \
